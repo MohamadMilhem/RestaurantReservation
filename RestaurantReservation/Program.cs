7a0fde0ed8decad39382ff26d05f50742351f52e
Mohammad Milhem <mohamadmilhem100@gmail.com>
2023-08-23 17:11:53 +0300

    Add and setup config

--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace RestaurantReservation
+﻿
+using Microsoft.IdentityModel.Protocols;
+using System.Configuration;
+
+namespace RestaurantReservation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
