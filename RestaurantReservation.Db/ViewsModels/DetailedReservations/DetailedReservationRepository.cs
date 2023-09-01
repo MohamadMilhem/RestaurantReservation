dd8a10052d9d48c1e8a5a425fe0b35e46fa16ed0
Mohammad Milhem <mohamadmilhem100@gmail.com>
2023-09-01 19:35:40 +0300

    Add DetailedReservation with its repository and service

---
+++ RestaurantReservation.Db/ViewsModels/DetailedReservations/DetailedReservationRepository.cs
@@ -0,0 +1,24 @@
+﻿using RestaurantReservation.Db.ViewsModels.DetailedEmpolyees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Db.ViewsModels.DetailedReservations
+{
+    public class DetailedReservationRepository
+    {
+        private readonly RestaurantReservationDbContext _dbContext;
+
+        public DetailedReservationRepository()
+        {
+            _dbContext = new RestaurantReservationDbContext();
+        }
+
+        public DetailedReservation GetDetailedReservationById(int Id)
+        {
+            return _dbContext.DetailedReservations.SingleOrDefault(res => res.ReservationId == Id);
+        }
+    }
+}
---
+++ RestaurantReservation.Db/ViewsModels/DetailedReservations/DetailedReservationService.cs
@@ -0,0 +1,25 @@
+﻿using RestaurantReservation.Db.ViewsModels.DetailedEmpolyees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Db.ViewsModels.DetailedReservations
+{
+    public class DetailedReservationService
+    {
+        private readonly DetailedReservationRepository _detailedReservationRepository;
+
+        public DetailedReservationService(DetailedReservationRepository detailedReservationRepository)
+        {
+            _detailedReservationRepository = detailedReservationRepository;
+        }
+
+        public DetailedReservation GetDetailedReservation(int id)
+        {
+            var reservation = _detailedReservationRepository.GetDetailedReservationById(id);
+            return reservation;
+        }
+    }
+}
