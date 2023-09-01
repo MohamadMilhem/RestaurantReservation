8410a9f5693632cfd36ca45100f85c01adb0e566
Mohammad Milhem <mohamadmilhem100@gmail.com>
2023-09-01 19:29:25 +0300

    Add Converter class

---
+++ RestaurantReservation.Db/Converter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Db
+{
+    public static class Converter
+    {
+        public static MenuChoice FromNumericStringToMenuChoice(string choice)
+        {
+            int value = int.Parse(choice);
+            return (MenuChoice)(value);
+        }
+
+        public static PartySize FromNumericStringToPartySize(string size)
+        {
+            int value = int.Parse(size);
+            return (PartySize)(value);
+        }
+    }
+}
