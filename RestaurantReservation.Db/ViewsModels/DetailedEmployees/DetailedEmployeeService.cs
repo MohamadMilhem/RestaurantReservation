497d86b5dae40f33bcea97fd8b5ab0cf73567a0d
Mohammad Milhem <mohamadmilhem100@gmail.com>
2023-09-01 19:34:50 +0300

    Add DetailedEmployee Class with its repository and service

---
+++ RestaurantReservation.Db/ViewsModels/DetailedEmployees/DetailedEmployeeService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Db.ViewsModels.DetailedEmpolyees
+{
+    public class DetailedEmployeeService
+    {
+        private readonly DetailedEmployeeRepository _detailedEmployeeRepository;
+
+        public DetailedEmployeeService(DetailedEmployeeRepository detailedEmployeeRepository)
+        {
+            _detailedEmployeeRepository = detailedEmployeeRepository;
+        }
+
+        public DetailedEmployee GetDetailedEmployee(int id)
+        {
+            var employee = _detailedEmployeeRepository.GetDetailedEmployeeById(id);
+            return employee;
+        }
+    }
+}
