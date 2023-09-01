125a32afbf6f26b8425c0a912a321ccb8e180604
Mohammad Milhem <mohamadmilhem100@gmail.com>
2023-09-01 19:30:31 +0300

    Add InputValidation Class

---
+++ RestaurantReservation.Db/InputValidation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Db
+{
+    public class InputValidation
+    {
+
+        public static bool ValidateMenuOption(string? option)
+        {
+            var canBeParsed = int.TryParse(option, out _);
+
+            if (!canBeParsed) 
+            {
+                return false;
+            }
+
+            return Enum.IsDefined(typeof(MenuChoice), int.Parse(option));
+        }
+
+        public static bool ValidateId(string? id)
+        {
+            var canBeParsed = int.TryParse(id, out _);
+            return canBeParsed;
+        }
+
+        public static bool ValidatePartySize(string? id)
+        {
+            var canBeParsed = int.TryParse(id, out _);
+
+            if (!canBeParsed)
+            {
+                return false;
+            }
+            return Enum.IsDefined(typeof(PartySize), int.Parse(id));
+        }
+    }
+}
